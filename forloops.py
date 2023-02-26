fb251ff2be56ddc8b3332f5246877e17c2841239
Dell <jamalnasirone52@gmail.com>
2023-02-25 17:44:46 -0800

    some additions

---
+++ forloops.py
@@ -0,0 +1,13 @@
+for letter in "Jamal Abdul Nasir":
+    print(letter)
+
+friends = ["Abdallah", "Bawa", "Theophilus"]
+
+for friend in friends:
+
+    print(friend)
+
+for index in range(len(friends)):
+
+    print(friends[index])
+
